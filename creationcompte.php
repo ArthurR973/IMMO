8b345079017e7114609e0ef63d6a2a7a1149ce77
Ton Nom <ton@email.com>
2024-05-30 15:48:49 +0200

    prion

--- creationcompte.php
+++ creationcompte.php
@@ -18,64 +18,6 @@
             border-radius: 10px;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
         }
-        .header {
-            background-color: #333;
-            color: white;
-            padding: 20px;
-            text-align: center;
-        }
-        .header h1 {
-            font-family: 'Montserrat', sans-serif;
-            color: white;
-            font-size: 2.5rem;
-            margin: 0;
-        }
-        .navigation {
-            background-color: #f8f9fa;
-            padding: 10px;
-            text-align: center;
-        }
-        .navigation a {
-            margin: 0 15px;
-            text-decoration: none;
-            color: #333;
-            font-weight: bold;
-        }
-        .welcome {
-            background-color: #f8f9fa;
-            color: #333;
-            padding: 50px;
-            text-align: center;
-        }
-        .welcome h2 {
-            font-family: 'Montserrat', sans-serif;
-            font-size: 2rem;
-            margin-bottom: 20px;
-        }
-        .welcome p {
-            font-size: 1rem;
-        }
-        .event-section {
-            padding: 50px 20px;
-            text-align: center;
-        }
-        .carousel-item img {
-            width: 100%;
-            height: auto;
-        }
-        .footer {
-            background-color: #333;
-            color: white;
-            text-align: center;
-            padding: 20px;
-        }
-        .error {
-            color: red;
-        }
-        p.terms {
-            font-size: 12px;
-            margin-top: 10px;
-        }
         .form-title {
             font-size: 24px;
             margin-bottom: 20px;
@@ -95,8 +37,9 @@
 </head>
 <body>
     <div class="container">
-    <a href="accueil.php">
-        <img src="logotest.png" alt="Logo Omnes Immobilier" class="logo"></a>
+        <a href="accueil.php">
+            <img src="logotest.png" alt="Logo Omnes Immobilier" class="logo">
+        </a>
         <h2 class="form-title">Créer un compte</h2>
         <form action="submitcreation.php" method="post">
             <div class="form-group">
@@ -125,7 +68,7 @@
             </div>
             <div class="form-group">
                 <label for="password">Mot de passe</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Au moins 3 chiffres" required>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Au moins 3 caractères" required>
             </div>
             <div class="form-group">
                 <label for="confirm_password">Confirmation du mot de passe</label>
